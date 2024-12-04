cf44f83c8da551ffbdbd716af6cf60ee5d2f504b
Ekaterina Fursina <fursinae@gmail.com>
2024-12-04 14:30:08 +0000

    Prolonged future +1

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ from prophet import Prophet
 m = Prophet()
 m.fit(df_timeseries.rename(columns={"start_time": "ds", "nb_disruptions": "y"}))
 
-future = m.make_future_dataframe(periods=375, freq="d")
+future = m.make_future_dataframe(periods=376, freq="d")
 
 forecast = m.predict(future)
 
