2e950e5f6b62a62c4bf239d5db565885d26c4eb6
Ekaterina Fursina <fursinae@gmail.com>
2024-12-04 14:38:40 +0000

    if not len()

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ st.write('Selected date is:', d)
 
 provinces = gdp_df['NUTS_2_0'].unique()
 
-if not len(provinces):
+if len(provinces) == 0:
     st.warning("Select at least one province!")
 
 selected_province = st.multiselect(
@@ -143,46 +143,3 @@ sentence = f'Predicted number of disruption in {selected_province} on {d} is {pr
 sentence
 
 ''
-
-
-
-#st.line_chart(
- #   filtered_gdp_df,
-  #  x='Year',
-   # y='GDP',
-    #color='Country Code',
-#)
-
-''
-''
-
-
-#first_year = gdp_df[gdp_df['Year'] == from_year]
-#last_year = gdp_df[gdp_df['Year'] == to_year]
-
-#st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-#cols = st.columns(4)
-
-#for i, country in enumerate(selected_countries):
- #   col = cols[i % len(cols)]
-
-  #  with col:
-   #     first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-    #    last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-#
- #       if math.isnan(first_gdp):
-   #         growth = 'n/a'
-  #          delta_color = 'off'
-    #    else:
-     #       growth = f'{last_gdp / first_gdp:,.2f}x'
-      #      delta_color = 'normal'
-
-       # st.metric(
-        #    label=f'{country} GDP',
-         #   value=f'{last_gdp:,.0f}B',
-          #  delta=growth,
-           # delta_color=delta_color
-        #)
